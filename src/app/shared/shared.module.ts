e3abde54c52a27d5adaed76419a0ac6f4c7028d9
smahli <mahlishiwshankar@gmail.com>
2022-08-30 21:57:21 +0530

    Frontend Designing

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchFormComponent } from '../components/search-form/search-form.component';
-
-
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
-  declarations: [
-    SearchFormComponent
-  ],
-  imports: [
-    CommonModule
-  ],
-  exports: [
-    SearchFormComponent
-  ]
+  declarations: [SearchFormComponent],
+  imports: [CommonModule, MatSelectModule],
+  exports: [SearchFormComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
